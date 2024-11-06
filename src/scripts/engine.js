a8e011f1df884c30cbdeea544af7eec39db34935
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-11-06 12:20:15 -0400

    hide worlds

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
@@ -34,3 +35,9 @@ const handleVolume = (e) => {
 }
 
 volumeSlider.addEventListener("input", handleVolume);
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+keysCheck.addEventListener("click", showHideKeys)
